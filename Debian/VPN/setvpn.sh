143f1b0bde1b00f6f9aade31ef4327a2beeae30d
Denis Sochi <lp85d@mail.ru>
2025-09-21 00:46:01 +0400

    Create setvpn.sh

---
+++ Debian/VPN/setvpn.sh
@@ -0,0 +1,50 @@
+UUID=$(/usr/local/bin/xray uuid)
+KEYS=$(/usr/local/bin/xray x25519)
+PRIVATE_KEY=$(echo "$KEYS" | grep "Private key" | awk '{print $3}')
+PUBLIC_KEY=$(echo "$KEYS" | grep "Public key" | awk '{print $3}')
+SHORT_ID=$(openssl rand -hex 8)
+DOMAIN="joyfultank.aeza.network"
+
+# Write config
+cat << EOF > /usr/local/etc/xray/config.json
+{
+  "log": { "loglevel": "info" },
+  "inbounds": [{
+    "listen": "0.0.0.0",
+    "port": 443,
+    "protocol": "vless",
+    "settings": {
+      "clients": [{
+        "id": "$UUID"
+      }],
+      "decryption": "none"
+    },
+    "streamSettings": {
+      "network": "tcp",
+      "security": "reality",
+      "realitySettings": {
+        "show": false,
+        "dest": "$DOMAIN:443",
+        "xver": 0,
+        "serverNames": ["$DOMAIN"],
+        "privateKey": "$PRIVATE_KEY",
+        "shortIds": ["$SHORT_ID"]
+      }
+    }
+  }],
+  "outbounds": [{
+    "protocol": "freedom"
+  }]
+}
+EOF
+
+systemctl restart xray
+
+echo "=== Connection data ==="
+echo "UUID:       $UUID"
+echo "PrivateKey: $PRIVATE_KEY"
+echo "PublicKey:  $PUBLIC_KEY"
+echo "ShortID:    $SHORT_ID"
+echo ""
+echo "VLESS link:"
+echo "vless://${UUID}@${DOMAIN}:443?security=reality&encryption=none&pbk=${PUBLIC_KEY}&type=tcp&sni=${DOMAIN}&sid=${SHORT_ID}#${DOMAIN}"
